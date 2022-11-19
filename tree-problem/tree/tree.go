c6e0c1273df9d915274ae3f11270c8f5827c7f27
Ramana Reddy <ramanareddy.manda@practo.com>
2022-11-19 13:05:23 +0530

    Fix to have exact tree structure

--- tree-problem/tree/tree.go
+++ tree-problem/tree/tree.go
@@ -26,31 +26,36 @@ type TreeConfig struct {
 	paths                                 []string
 }
 
+const (
+	//Box Drawing Characters
+	BoxVer        = "│"
+	BoxHor        = "──"
+	BoxVH         = BoxVer + BoxHor
+	BoxDowAndRig  = "┌"
+	BoxDowAndLef  = "┐"
+	BoxUpAndRig   = "└"
+	BoxUpAndLef   = "┘"
+
+	OpenBrkt      = "["
+	CloseBrkt     = "]"
+	Command       = "tree"
+	PathSeperator = string(os.PathSeparator)
+	NewLine       = "\n"
+	Space         = " "
+	Spaces3       = "   "
+	Spaces4       = "    "
+)
+
 func NewTreeConfig() *TreeConfig {
 	config := new(TreeConfig)
 	return config
 }
 
-const (
-	//Box Drawing Characters
-	BoxVer       = "│"
-	BoxHor       = "──"
-	BoxVH        = BoxVer + BoxHor
-	BoxDowAndRig = "┌"
-	BoxDowAndLef = "┐"
-	BoxUpAndRig  = "└"
-	BoxUpAndLef  = "┘"
-
-	Command = "tree"
-	Spaces3 = "	  "
-	Spaces4 = Spaces3 + " "
-)
-
 func ParseCommand(cmd string) TreeConfig {
 	regxCmpl := regexp.MustCompile(`\s+`)
 	cmd = strings.TrimSpace(regxCmpl.ReplaceAllString(cmd, " "))
 
-	ca := strings.Split(cmd, " ") //args in command
+	ca := strings.Split(cmd, Space) //args in command
 
 	if strings.TrimSpace(ca[0]) != Command {
 		log.Fatalf("command not found: `%v`", ca[0])
@@ -60,12 +65,6 @@ func ParseCommand(cmd string) TreeConfig {
 
 	for i := 1; i < len(ca); i++ {
 		v := ca[i]
-
-		// check for path
-		if !strings.HasPrefix(v, "-") && !regexp.MustCompile(`\d`).MatchString(v) {
-			config.paths = append(config.paths, v)
-			continue
-		}
 
 		op := v //op: option
 		switch op {
@@ -86,6 +85,11 @@ func ParseCommand(cmd string) TreeConfig {
 		case "-p":
 			config.reqFilePermsn = true
 		default:
+			// check for path
+			if !strings.HasPrefix(v, "-") {
+				config.paths = append(config.paths, v)
+				continue
+			}
 			log.Fatalf("Invalid argument `%v`", op)
 		}
 	}
@@ -96,97 +100,18 @@ func ParseCommand(cmd string) TreeConfig {
 	return *config
 }
 
-func parseToInt(input string) int {
-	num, err := strconv.ParseInt(input, 10, 32)
-	//check for error
-	if err != nil {
-		log.Println(err.Error())
-		return 0
-	}
-	return int(num)
-}
-
 func ListDirAndFiles(config TreeConfig) string {
 	var fc FileCount
+	var isNthDirLast []bool
 	temp := ""
+	
 	for _, p := range config.paths {
 		fc = FileCount{}
-		root := strings.TrimSuffix(p, "/")
-		temp += recListDirAndFiles(root, root+"\n", 0, false, &fc, &config)
+		isNthDirLast = []bool{}
+		root := strings.TrimSuffix(p, PathSeperator)
+		temp += recListDirAndFiles(root, root+NewLine, 0, &isNthDirLast, &fc, &config)
 	}
-
-	return fmt.Sprintf("%v \n %v directory, %v files", temp, fc.dirCnt, fc.fileCnt)
-}
-
-func recListDirAndFiles(root string, temp string, n int, isLastDir bool, fc *FileCount, config *TreeConfig) string {
-	files := ReadDir(root)
-
-	if len(files) < 1 || (config.level > 0 && n == config.level) {
-		return temp
-	}
-
-	lastFile := files[len(files)-1]
-
-	for _, val := range files {
-		//ignoring file start with `.`
-		if strings.HasPrefix(val.Name(), ".") {
-			continue
-		}
-
-		bp := "" // before pipe
-		pipe := BoxVH
-		ap := " " + val.Name() //after pipe
-		isLastFile := lastFile == val
-		var relPath, fp string // fp: file permission
-
-		if config.reqRelPath {
-			relPath = " " + root + "/" + val.Name()
-			ap = relPath
-		}
-		if config.reqFilePermsn {
-			fp = " [" + getPermsnMode(val) + "] "
-			ap = fp + val.Name()
-		}
-		if config.reqRelPath && config.reqFilePermsn {
-			ap = fp + relPath
-		}
-
-		if isLastFile {
-			pipe = BoxUpAndRig + BoxHor
-		}
-
-		if n > 0 {
-			bp = BoxVer + "   " + strings.Repeat("    ", n-1)
-		}
-
-		if n == 1 && isLastDir {
-			bp = strings.Repeat("    ", n)
-		}
-
-		if n > 1 && !isLastDir {
-			bp = BoxVer + "   " + strings.Repeat("    ", n-2) + BoxVer + "   "
-		}
-
-		if n > 1 && isLastDir {
-			bp = BoxVer + "   " + strings.Repeat("    ", n-1)
-		}
-
-		if !val.IsDir() { // file
-			if config.reqOnlyDir {
-				continue
-			}
-			temp += bp + pipe + ap + "\n"
-			fc.fileCnt++
-			continue
-		}
-
-		isLastDir = isLastFile && lastFile.IsDir()
-		fc.dirCnt++
-		temp += bp + pipe + ap + "\n"
-		temp = recListDirAndFiles(root+"/"+val.Name(), temp, n+1, isLastDir, fc, config)
-	}
-
-	return temp
+	return fmt.Sprintf("%v %v %v directory, %v files", temp, NewLine, fc.dirCnt, fc.fileCnt)
 }
 
 func ReadDir(root string) []fs.DirEntry {
@@ -196,15 +121,6 @@ func ReadDir(root string) []fs.DirEntry {
 		return make([]fs.DirEntry, 0)
 	}
 	return files
-}
-
-func getPermsnMode(f fs.DirEntry) string {
-	fi, err := f.Info()
-	if err != nil {
-		fmt.Println(err)
-		return "Unable read mode"
-	}
-	return fi.Mode().String()
 }
 
 func OrderDirAndFiles(files []fs.DirEntry) []fs.DirEntry {
@@ -224,6 +140,110 @@ func OrderDirAndFiles(files []fs.DirEntry) []fs.DirEntry {
 		df["files"] = append(df["files"], f)
 	}
 	return append(df["directories"], df["files"]...)
+}
+
+func recListDirAndFiles(root string, temp string, n int, isNthDirLast *[]bool, fc *FileCount, config *TreeConfig) string {
+	files := ReadDir(root)
+
+	if len(files) < 1 || (config.level > 0 && n == config.level) {
+		return temp
+	}
+
+	lastFile := files[len(files)-1]
+
+	for _, f := range files {
+		//ignoring file start with `.`
+		if strings.HasPrefix(f.Name(), ".") {
+			continue
+		}
+
+		bp := "" // before pipe
+		pipe := BoxVH
+		ap := Space + f.Name() //after pipe
+		isLastFile := lastFile == f
+
+		ap = getAfterPipeVal(root, f, *config, ap)
+
+		if isLastFile {
+			pipe = BoxUpAndRig + BoxHor
+		}
+
+		bp = getBeforePipeVal(n, *isNthDirLast)
+
+		if !f.IsDir() { // file
+			if config.reqOnlyDir {
+				continue
+			}
+			temp += bp + pipe + ap + NewLine
+			fc.fileCnt++
+			continue
+		}
+		//directory
+		temp += bp + pipe + ap + NewLine
+		fc.dirCnt++
+		//tracking information(whether directory last or not) from 0 to Nth level directory
+		*isNthDirLast = append(*isNthDirLast, isLastFile)
+		temp = recListDirAndFiles(root+PathSeperator+f.Name(), temp, n+1, isNthDirLast, fc, config)
+	}
+
+	if n > 0 && len(*isNthDirLast) > 0 {
+		*isNthDirLast = (*isNthDirLast)[:n-1]
+	}
+	return temp
+}
+
+func getAfterPipeVal(root string, fi fs.DirEntry, config TreeConfig, ap string) string {
+	var relPath, fp string // fp: file permission
+	if config.reqRelPath {
+		relPath = Space + root + PathSeperator + fi.Name()
+		ap = relPath
+	}
+
+	if config.reqFilePermsn {
+		fp = Space + OpenBrkt + getPermsnMode(fi) + CloseBrkt + Space
+		ap = fp + fi.Name()
+	}
+
+	if config.reqRelPath && config.reqFilePermsn {
+		ap = fp + relPath
+	}
+	return ap
+}
+
+func getPermsnMode(f fs.DirEntry) string {
+	fi, err := f.Info()
+	if err != nil {
+		fmt.Println(err)
+		return "Unable read mode"
+	}
+	return fi.Mode().String()
+}
+
+func getBeforePipeVal(n int, isNthDirLast []bool) string {
+	bp := ""
+
+	if n < 1 || n > len(isNthDirLast) {
+		return bp
+	}
+
+	for i := 0; i < n; i++ {
+		if !isNthDirLast[i] {
+			bp += BoxVer + Spaces3
+			continue
+		}
+		bp += Spaces4
+	}
+	return bp
+}
+
+func parseToInt(input string) int {
+	num, err := strconv.ParseInt(input, 10, 32)
+	//check for error
+	if err != nil {
+		log.Println(err.Error())
+		return 0
+	}
+	return int(num)
 }
 
 func help() {
